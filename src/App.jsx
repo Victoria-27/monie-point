1660590bcc259b555c9702ca0c1aa1789f644d1d
Victoria Abioye <victoriousvikky003@gmail.com>
2024-03-11 14:47:32 +0100

    fix: first page fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
   const [count, setCount] = useState(0);
   const [screen, setScreen] = useState(0);
 
-  const Gridarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3,4, 5, 6, 7, 8, 9];
+  const Gridarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 34, 5, 6, 7, 8, 9];
   function transitionGenerator() {
     if (count === 1) {
       return "tansition_one";
     } else if (count === 2) {
-      return "transition_two";
+      return "tansition_one transition_two";
     } else if (count === 3) {
-      return "transition_three";
+      return " transition_three";
     } else if (count === 4) {
       return "transition_four ";
     } else if (count === 5) {
@@ -26,7 +26,6 @@ function App() {
     } else if (count === 8) {
       
     }
-    // return "transition_five ";
   }
   const ScreenGEnerator = () => {
     if (screen === 1) {
@@ -48,10 +47,7 @@ function App() {
       play();
     }
   }, [screen]);
-  // useEffect(()=>{
-  //      playScreen()
 
-  // },[])
   console.log(count);
   const play = () => {
     let i = count;
@@ -59,63 +55,50 @@ function App() {
     const intervalId = setInterval(() => {
       if (i >= 9) {
         clearInterval(intervalId);
-        setScreen((prevScreen) => prevScreen + 1); // Increment screen state by 1
+        setScreen((prevScreen) => prevScreen + 1);
       } else {
         setCount(i);
         i++;
       }
     }, delay);
   };
-  // const  playScreen = () => {
-  //   let i = screen;
-  //   const delay = 700;
 
-  //   const intervalId = setInterval(() => {
-  //     if (i >= 6) {
-  //       clearInterval(intervalId);
-  //     } else {
-  //     (setScreen(i));
-  //       i++;
-  //     }
-  //   }, delay);
-  // };
-
-  const counter = () => {
-    if (count < 5) {
-      setCount((cou) => cou + 1);
-    }
-  };
   return (
     <>
       <section
         className={`app ${ScreenGEnerator()}  w-screen h-screen overflow-hidden`}
       >
-        {count <8 && (
-          <main className={`table ${transitionGenerator()}  bg-yellow-50 w-screen h-screen overflow-hidden`}>
+         
+         {count <8 && ( <main className={`table ${transitionGenerator()}  bg-amber-200 w-screen h-screen overflow-hidden`}>
             <div className="genbox">
-              <div className="icon_1 text-orange-800">
-                {count > 2 ? "↓" : "↑"}
+              <div className="icon_1 text-orange-800 text-5xl">
+                {/* {count > 2 ? "↓" : "↑"} */} →
               </div>
-              <div data-count={count} className="icon_2  text-orange-200">
-                {count !== 0 ? "↓" : "DENTAL"}
+              <div className="icon_2  text-orange-200 text-5xl ml-2">
+                <span className="px-3">
+                  
+                </span>
               </div>
             </div>
-            <div className="gen2 ">
-              <div data-count={count}  className="icon_3 text-orange-800">
-                {count > 1 ? "↑" : "ASSISTANCE"}
+            <div className="gen2 text-5xl">
+              <div className="icon_3 text-orange-800 mr-2">
+              <span className="px-3">
+                  
+                </span>
+                
               </div>
-              <div className="icon_4">👱 </div>
-              <div className="overflow-hidden note_wrapper">
-                <p className="note">DENTYTECH</p>
+              <div className="icon_4">🦷</div>
+              <div className="overflow-hidden note_wrapper ">
+                <p className="note ">DENTYTECH</p>
               </div>
             </div>
 
             {Gridarr.map((entries, index) => {
               return <div key={index} className="box transition-three"></div>;
             })}
-          </main>
-        )}
-        {count === 8?
+          </main>)}
+       
+         {count === 8?
           <LandingPg/>
         :<></>}
       </section>
